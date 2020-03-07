69a8ac047c61ccbceea069e958fa1fbe438aacab
Razziat <56124872+Razziat@users.noreply.github.com>
2020-03-07 17:21:06 +0100

    legere modif de script Manager
    J'ai juste supprimer les lignes de codes en commentaire du script manager

--- Jeu/Assets/Scripts/Manager.cs
+++ Jeu/Assets/Scripts/Manager.cs
@@ -8,7 +8,7 @@ namespace Com.TestMulti.SimpleHostile
     public class Manager : MonoBehaviourPunCallbacks
     {
         public string player_prefab;
-        //public GameObject player_prefab;
+       
         public Transform spawn_point;
 
         private void Start()
@@ -19,18 +19,7 @@ namespace Com.TestMulti.SimpleHostile
         public void Spawn ()
         {
             PhotonNetwork.Instantiate(player_prefab, spawn_point.position, spawn_point.rotation);
-            //Transform t_spawn = spawn_points[Random.Range(0, spawn_points.Length)];
-
-            //if (PhotonNetwork.IsConnected)
-            //{
-           
-            //}
-
-            /*else
-            {
-                Debug.Log("WORKING");
-                GameObject newPlayer = Instantiate(player_prefab, t_spawn.position, t_spawn.rotation) as GameObject;
-            }*/
+            
         }
     }
 }
