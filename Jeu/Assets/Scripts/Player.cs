9f09a7e77602572f57be07b0e25a395a12cbdbba
Alexis DUCHET <59805596+Ratcraft@users.noreply.github.com>
2020-03-09 18:10:10 +0100

    Jambes et Vie

---
+++ Jeu/Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine.Networking;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private int playerHp;
+    private int playerdmg;
+    private int maxplayerhp;
+    private int speed;
+
+    public int GetPlayerHp => playerHp;
+    public int GetPlayerDmg => playerdmg;
+
+    bool PlayerDead = false;
+
+
+    public Player(int hpmax)
+    {
+        this.maxplayerhp = hpmax;
+        this.playerHp = maxplayerhp;
+        this.speed = 10;
+    }
+
+
+    public void Damage(int dmg)
+    {
+        playerHp -= dmg;
+        if (!PlayerDead)
+        {
+            if (playerHp <= 0)
+            {
+                playerHp = 0;
+                PlayerDead = true;
+                Debug.Log("Dead");
+            }
+        }
+    }
+
+    public void Heal(int heal)
+    {
+        playerHp += heal;
+        if (playerHp > maxplayerhp)
+            playerHp = maxplayerhp;
+    }
+
+}
