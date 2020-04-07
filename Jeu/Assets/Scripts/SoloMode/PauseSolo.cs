f24985cd6930f717d61751be021dda085d82bab0
Razziat <56124872+Razziat@users.noreply.github.com>
2020-04-07 21:04:39 +0200

    ajout du menu pause pour le solo

---
+++ Jeu/Assets/Scripts/SoloMode/PauseSolo.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseSolo : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+
+        if(isPaused)
+        {
+            ActivateMenu();
+        }
+        else
+        {
+            DesactivateMenu();
+        }
+    }
+
+    public void ActivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = true;
+        pauseMenuUI.SetActive(true);
+    }
+
+   public void DesactivateMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
