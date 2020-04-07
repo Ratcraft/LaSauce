cfee4225a770cbaae80bd5c3d413ce206b95bc99
Razziat <56124872+Razziat@users.noreply.github.com>
2020-04-07 15:22:19 +0200

    ajout d'une ia de base dans le mode solo

---
+++ Jeu/Assets/Scripts/SoloMode/Player/SoloMouvement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.TestMulti.SimpleHostile
+{
+    public class SoloMouvement : MonoBehaviour
+    {
+        public float speed = 10f;
+        public float jumpForce = 8f;
+        private float gravity = 30f;
+        private Vector3 moveDir = Vector3.zero;
+
+        void Start()
+        {
+
+        }
+
+        
+        void Update()
+        {
+            CharacterController controller = gameObject.GetComponent<CharacterController>();
+
+            if(controller.isGrounded)
+            {
+                moveDir = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+                moveDir = transform.TransformDirection(moveDir);
+
+                moveDir *= speed;
+
+                if(Input.GetButtonDown("Jump"))
+                {
+                    moveDir.y = jumpForce;
+                }
+                
+
+            }
+
+            moveDir.y -= gravity * Time.deltaTime;
+
+            controller.Move(moveDir * Time.deltaTime);
+
+        }
+    }
+}
