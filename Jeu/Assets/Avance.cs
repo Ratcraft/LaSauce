b4f5ec89e17da41e56eefc656ca4f819d27599ac
MkGuL <61564525+MkGuL@users.noreply.github.com>
2020-05-21 16:23:07 +0200

    Ajout Map Plage enfin

---
+++ Jeu/Assets/Avance.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avance : MonoBehaviour {
+
+    public float speed = 2000;
+    private Rigidbody rb;
+    public Transform spawnPos;
+
+
+    // Use this for initialization
+    private void Awake() {
+        rb = this.GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * Time.deltaTime * speed;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+    	int i = 0;
+    	while(i<50)
+    	{
+			if((transform.position.x < -48) || (transform.position.x > 98))
+			{
+            	Destroy(this.gameObject);
+           		Instantiate(this.gameObject, spawnPos.position, spawnPos.rotation);
+        	}
+        	i++;
+       	}
+    }
+}
